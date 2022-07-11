98f9b2d3671bb53c55503b549675223c1e3c78cd
Paulo Henrique Luz dos Santos <i309843@fb.org.br>
2022-07-10 21:23:02 -0300

    Salvando exercício 2

---
+++ MaquinaDinheiro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercicio2
+{
+    public static class MaquinaDinheiro
+    {
+        public static void CalculaCedulas(int valorSaque, ref List<Nota> lstCedulas)
+        {
+            // Obtém padrão de notas
+            var listaNotas = new List<Nota>()
+            {
+                new Nota(){Valor = 10},
+                new Nota(){Valor = 20},
+                new Nota(){Valor = 50},
+                new Nota(){Valor = 100}
+            };
+
+            // Calcula maior nota
+            Nota maiorNota = listaNotas.FirstOrDefault();
+            foreach (Nota nota in listaNotas)
+                if (valorSaque >= nota.Valor)
+                    maiorNota = nota;
+
+            // Adiciona notas na lista de retorno conforme divisão da maior nota
+            for (int i = 0; i < valorSaque / maiorNota.Valor; i++)
+                lstCedulas.Add(maiorNota);
+
+            // Varifica se sobrou valor e chama o método novamente de forma recursiva
+            var valorRestante = valorSaque % maiorNota.Valor;
+            if ((valorRestante != 0) && (valorRestante >= 10))
+                CalculaCedulas(valorRestante, ref lstCedulas);
+        }
+
+        public static void Sacar(List<Nota> notas)
+        {
+            // REALIZA O SAQUE DAS NOTAS 
+            foreach (Nota nota in notas)
+                Console.WriteLine($"SACANDO NOTA {nota.Valor}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int valorSaque = 0;
+                while (valorSaque == 0)
+                {
+                    Console.Write("Digite um valor para saque: ");
+                    string valor = Console.ReadLine();
+                    if (int.TryParse(valor, out valorSaque))
+                    {
+                        List<Nota> lstCedulas = new List<Nota>();
+                        MaquinaDinheiro.CalculaCedulas(valorSaque, ref lstCedulas);
+                        MaquinaDinheiro.Sacar(lstCedulas);
+                    }
+                    else
+                        throw new ArgumentException("Valor inválido");
+                }
+            }
+            catch(ArgumentException aex) {
+                Console.WriteLine(aex.Message);
+            }
+            catch (Exception){
+                Console.WriteLine("Ocorreu um erro ao sacar cédulas");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
